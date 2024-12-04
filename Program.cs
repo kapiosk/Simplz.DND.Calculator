f35e92baaa4031a17421756e441b42a501452b7c
AZ <speedsterk@hotmail.com>
2024-12-04 13:08:22 +0200

    Add random roll simulation and output totals

--- Program.cs
+++ Program.cs
@@ -2,6 +2,23 @@ using Simplz.DND.Calculator.Components;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var rolls = 10;
+
+long a1Total = 0;
+long a2Total = 0;
+foreach(int i in Enumerable.Range(0, rolls))
+{
+    var a1 = Random.Shared.NextInt64(1, 9);
+    if (a1 == 8)
+        a1 += Random.Shared.NextInt64(1, 9);
+    var a2 = Random.Shared.NextInt64(1, 11);
+    a1Total += a1;
+    a2Total += a2;
+    Console.WriteLine($"a1: {a1}, a2: {a2}");
+}
+Console.WriteLine($"a1: {a1Total} ({a1Total / rolls})");
+Console.WriteLine($"a2: {a2Total} ({a2Total / rolls})");
+
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
